dbe2177f113583d06d84ac83051436b6bb43f245
JohnHemann <john.hemann2@g.austincc.edu>
2019-05-18 11:00:26 -0500

    added to code

--- Aurdino/avr/ping/config.h
+++ Aurdino/avr/ping/config.h
@@ -5,6 +5,10 @@
 #define PING_DIR     _(DDRD)
 #define PING_READ    _(PIND)
 
+#define LED_PIN	     5
+#define LED_PORT     _(PORTB)
+#define LED_DIR      _(DDRB)
+
 // include this line to avoid SFR_REG issues
 #define _(s)    _SFR_IO_ADDR(s)
 
